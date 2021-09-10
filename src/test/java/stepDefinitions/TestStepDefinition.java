94c28143926f5aff10fb745689b96edbde8a0c27
midhun288622 <midhun.c.devassy@gmail.com>
2021-09-10 20:09:05 +0530

    cxvx

--- src/test/java/stepDefinitions/TestStepDefinition.java
+++ src/test/java/stepDefinitions/TestStepDefinition.java
@@ -51,9 +51,8 @@ public class TestStepDefinition {
 	public void i_want_to_authenticate_with_valid_credentails() {
 
 		System.out.println("Start updating the job 1 data");
-		System.out.println(System.getProperty("user.dir"));
 		String previousJobDir=System.getProperty("user.dir").replaceAll("CucumberTest2", "CucumberTest1");
-		//System.out.println(Utils.fileReader(previousJobDir+"/PNR", "AB_BOOKING_CB"));
+		System.out.println(Utils.fileReader(previousJobDir+"/PNR/", "AB_BOOKING_CB"));
 		System.out.println(previousJobDir);
 		System.out.println("Start updating the job 1 data");
 		
