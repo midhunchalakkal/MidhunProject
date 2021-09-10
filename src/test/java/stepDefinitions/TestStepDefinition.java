f72ee4ad8eb2e200d368ae334287a1f0a642953c
midhun288622 <midhun.c.devassy@gmail.com>
2021-09-10 15:01:21 +0530

    sdvds

--- src/test/java/stepDefinitions/TestStepDefinition.java
+++ src/test/java/stepDefinitions/TestStepDefinition.java
@@ -72,7 +72,6 @@ public class TestStepDefinition {
 			System.out.println("#####################################");
 			System.out.println("                                     ");
 			System.out.println("Test case is passsed");
-			System.out.println(System.getProperty("Username"));
 			System.out.println(Configuration.getConfiguration().getProperty("Username"));
 			System.out.println("                                     ");
 			System.out.println("#####################################");
